2c19d5a5c722b76d7214c98364f79965e5274f9b
Eric Rochester <err8n@virginia.edu>
2011-04-14 13:37:06 -0400

    Added header and cleaned up datastream admin browse view.

--- config_form.php
+++ config_form.php
@@ -1,3 +1,43 @@
+<?php
+/* vim: set expandtab tabstop=4 shiftwidth=4 softtabstop=4; */
+
+/**
+ * FedoraConnector Omeka plugin allows users to reuse content managed in
+ * institutional repositories in their Omeka repositories.
+ *
+ * The FedoraConnector plugin provides methods to generate calls against Fedora-
+ * based content disemminators. Unlike traditional ingestion techniques, this
+ * plugin provides a facade to Fedora-Commons repositories and records pointers
+ * to the "real" objects rather than creating new physical copies. This will
+ * help ensure longer-term durability of the content streams, as well as allow
+ * you to pull from multiple institutions with open Fedora-Commons
+ * respositories.
+ *
+ * PHP version 5
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at http://www.apache.org/licenses/LICENSE-2.0 Unless required by
+ * applicable law or agreed to in writing, software distributed under the
+ * License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS
+ * OF ANY KIND, either express or implied. See the License for the specific
+ * language governing permissions and limitations under the License.
+ *
+ * @package     omeka
+ * @subpackage  fedoraconnector
+ * @author      Scholars' Lab <>
+ * @author      Ethan Gruber <ewg4x@virginia.edu>
+ * @author      Adam Soroka <ajs6f@virginia.edu>
+ * @author      Wayne Graham <wayne.graham@virginia.edu>
+ * @author      Eric Rochester <err8n@virginia.edu>
+ * @copyright   2010 The Board and Visitors of the University of Virginia
+ * @license     http://www.apache.org/licenses/LICENSE-2.0.html Apache 2 License
+ * @version     $Id$
+ * @link        http://omeka.org/add-ons/plugins/FedoraConnector/
+ * @tutorial    tutorials/omeka/FedoraConnector.pkg
+ */
+?>
+
 <div class="field">
     <label for="fedora_connector_server">Omitted Datastreams:</label>
     <?php echo $input; ?>
@@ -5,3 +45,14 @@
 omitted from the datastream selection checkbox list and object metadata 
 dropdown menu.  Default: RELS-EXT,RELS-INT,AUDIT.  </p>
 </div>
+
+<?php
+/*
+ * Local variables:
+ * tab-width: 4
+ * c-basic-offset: 4
+ * c-hanging-comment-ender-p: nil
+ * End:
+ */
+
+?>
