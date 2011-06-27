cc64ce0399a898aa93e8850af775adede15b0ceb
David McClure <david.mcclure@virginia.edu>
2011-06-27 09:48:15 -0400

    changed omitted streams textbox in config form to textarea

--- config_form.php
+++ config_form.php
@@ -40,13 +40,15 @@
 ?>
 
 <div class="field">
+
     <label for="fedora_connector_server">Omitted Datastreams:</label>
-    <?php echo __v()->formText('fedora_connector_omitted_datastreams',
-        get_option('fedora_connector_omitted_datastreams'), array('size' => 30)); ?>
+    <?php echo __v()->formTextArea('fedora_connector_omitted_datastreams',
+        get_option('fedora_connector_omitted_datastreams'), array('rows' => 3, 'cols' => 40)); ?>
 
     <p class="explanation">List datastream IDs, comma-separated, that should be 
         omitted from the datastream selection checkbox list and object metadata 
         dropdown menu. Default: RELS-EXT,RELS-INT,AUDIT.</p>
+
 </div>
 
 <?php
