f9a44c01dc10e28ab9c0a44ae5da9303ef6f4cc0
David McClure <david.mcclure@virginia.edu>
2011-06-28 16:20:04 -0400

    got rid of vestigial bagit <p> in datastream delete template

--- views/admin/datastreams/delete.php
+++ views/admin/datastreams/delete.php
@@ -5,7 +5,6 @@
     <?php echo flash(); ?>
 
     <h2>Are you sure you want to delete the datastream "<?php echo $datastream->datastream; ?>" (PID <?php echo $datastream->pid ?>)?</h2>
-    <p>The files themselves won't be deleted - just the record of the grouping of the files represented by the collection.</p>
 
     <form action="<?php echo uri('/fedora-connector/datastreams/' . $datastream->id . '/delete'); ?>" method="post" class="button-form">
       <input type="hidden" name="confirm" value="true" />
