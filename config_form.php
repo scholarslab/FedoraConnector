d1842361f9a2d8d82145618fab302f3de0d7c9c0
David McClure <david.mcclure@virginia.edu>
2011-07-02 14:09:38 -0400

    changed out possible problematic __v()-> call in config_form.php

--- config_form.php
+++ config_form.php
@@ -42,8 +42,8 @@
 <div class="field">
 
     <label for="fedora_connector_server">Omitted Datastreams:</label>
-    <?php echo __v()->formTextArea('fedora_connector_omitted_datastreams',
-        get_option('fedora_connector_omitted_datastreams'), array('rows' => 3, 'cols' => 40)); ?>
+
+    <textarea name="fedora_connector_omitted_datastreams" id="fedora_connector_omitted_datastreams" rows="3" cols="40"><?php echo get_option('fedora_connector_omitted_datastreams'); ?></textarea>
 
     <p class="explanation">List datastream IDs, comma-separated, that should be 
         omitted from the datastream selection checkbox list and object metadata 
