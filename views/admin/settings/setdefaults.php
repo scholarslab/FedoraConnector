980bfa668dd0c3c723998c287ea37629880b2452
David McClure <david.mcclure@virginia.edu>
2011-07-21 10:03:11 -0400

    added basic view framework for settings

---
+++ views/admin/settings/setdefaults.php
@@ -0,0 +1,11 @@
+<?php echo $this->partial('datastreams/admin-header.php', array('subtitle' => 'Import Settings')); ?>
+
+<div id="primary">
+
+    <?php echo flash(); ?>
+
+
+
+</div>
+
+<?php foot(); ?>
