03e22bdd76cbbf19fa22ba0b42d010a49417993d
David McClure <david.mcclure@virginia.edu>
2011-08-04 13:37:28 -0400

    styling edit on item datastreams listing

--- forms/item_import_form.php
+++ forms/item_import_form.php
@@ -5,16 +5,16 @@
 
     <table>
         <thead>
-            <td>Datastream</th>
-            <td>PID</th>
-            <td>Server</th>
-            <td>Format</th>
-            <td>Import?</th>
+            <td><strong>Datastream</strong></th>
+            <td><strong>PID</strong></th>
+            <td><strong>Server</strong></th>
+            <td><strong>Format</strong></th>
+            <td><strong>Import?</strong></th>
 
         <?php foreach($datastreams as $datastream): ?>
 
             <tr>
-                <td class="fedora-td-small"><strong><a href="<?php echo $datastream->getUrl(); ?>"><?php echo $datastream->getNode()->getAttribute('label'); ?></a></strong></td>
+                <td width="30%" class="fedora-td-small"><strong><a href="<?php echo $datastream->getUrl(); ?>"><?php echo $datastream->getNode()->getAttribute('label'); ?></a></strong></td>
                 <td class="fedora-td-small"><?php echo $datastream->pid; ?></td>
                 <td class="fedora-td-small"><a href="<?php echo uri('/fedora-connector/servers/edit/' . $datastream->server_id); ?>"><?php echo $datastream->server_name; ?></a></td>
                 <td class="fedora-td-small"><?php echo $datastream->metadata_stream; ?></td>
