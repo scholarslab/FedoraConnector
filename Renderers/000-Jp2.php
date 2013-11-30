58240ec9420585e47470d27c71548a0fe6faf00d
David McClure <david.mcclure@virginia.edu>
2013-11-29 16:43:20 -0800

    Style cleanup on renderers.

--- Renderers/000-Jp2.php
+++ Renderers/000-Jp2.php
@@ -1,44 +1,42 @@
 <?php
-/* vim: set expandtab tabstop=4 shiftwidth=4 softtabstop=4; */
+
+/* vim: set expandtab tabstop=4 shiftwidth=4 softtabstop=4 cc=80; */
 
 /**
- * JPEG-2000 renderer.
- *
  * @package     omeka
- * @subpackage  fedoraconnector
- * @author      Scholars' Lab <>
- * @author      David McClure <david.mcclure@virginia.edu>
- * @copyright   2012 The Board and Visitors of the University of Virginia
- * @license     http://www.apache.org/licenses/LICENSE-2.0.html Apache 2 License
+ * @subpackage  fedora-connector
+ * @copyright   2012 Rector and Board of Visitors, University of Virginia
+ * @license     http://www.apache.org/licenses/LICENSE-2.0.html
  */
 
 
 class Jp2_Renderer extends FedoraConnector_AbstractRenderer
 {
 
+
     /**
      * Check of the renderer can handle a given mime type.
      *
      * @param string $mimeType The mimeType.
-     *
      * @return boolean True if this can display the datastream.
      */
     function canDisplay($mimeType)
     {
-        return ($mimeType == 'image/jp2');
+        return $mimeType == 'image/jp2';
     }
 
+
     /**
-     * This displays an object.
+     * Displays an object.
      *
      * @param Omeka_Record $object The Fedora object record.
-     *
      * @return string The display HTML for the datastream.
      */
     function display($object, $params = array())
     {
         return $this->_display($object, $params);
     }
+
 
     /**
      * Displays the image.
@@ -51,11 +49,8 @@ class Jp2_Renderer extends FedoraConnector_AbstractRenderer
     private function _display($object, $params = array())
     {
 
-        if (empty($params)) {
-          $params = array(
-            'scale' => '400,0'
-          );
-        }
+        // Default parameters.
+        if (empty($params)) $params = array('scale' => '400,0');
 
         // Get server.
         $server = $object->getServer();
@@ -64,9 +59,10 @@ class Jp2_Renderer extends FedoraConnector_AbstractRenderer
         $url = "{$server->url}/{$server->getService()}/{$object->pid}"
           . "/methods/djatoka:jp2SDef/getRegion?". http_build_query($params);
 
-        // Construct HTML.
+        // Return the image tag.
         return "<img class='fedora-renderer' alt='image' src='{$url}' />";
 
     }
+
 
 }
--- Renderers/100-Image.php
+++ Renderers/100-Image.php
@@ -1,47 +1,52 @@
 <?php
-/* vim: set expandtab tabstop=4 shiftwidth=4 softtabstop=4; */
+
+/* vim: set expandtab tabstop=4 shiftwidth=4 softtabstop=4 cc=80; */
 
 /**
- * image/* renderer.
- *
  * @package     omeka
- * @subpackage  fedoraconnector
- * @author      Scholars' Lab <>
- * @author      David McClure <david.mcclure@virginia.edu>
- * @copyright   2012 The Board and Visitors of the University of Virginia
- * @license     http://www.apache.org/licenses/LICENSE-2.0.html Apache 2 License
+ * @subpackage  fedora-connector
+ * @copyright   2012 Rector and Board of Visitors, University of Virginia
+ * @license     http://www.apache.org/licenses/LICENSE-2.0.html
  */
 
 
 class Image_Renderer extends FedoraConnector_AbstractRenderer
 {
 
+
     private static $_excludedFormats = array(
         'jp2', 'x-mrsid-image'
     );
+
 
     /**
      * Check of the renderer can handle a given mime type.
      *
      * @param string $mimeType The mimeType.
-     *
      * @return boolean True if this can display the datastream.
      */
     function canDisplay($mimeType) {
-        $excludeString = implode('|', self::$_excludedFormats);
-        return (bool)(preg_match('/^image\/(?!'.$excludeString.')/', $mimeType));
+        $exclude = implode('|', self::$_excludedFormats);
+        return (bool)(preg_match('/^image\/(?!'.$exclude.')/', $mimeType));
     }
+
 
     /**
      * Display an object.
      *
      * @param Omeka_Record $object The Fedora object record.
-     *
      * @return string The display HTML for the datastream.
      */
     function display($object, $params = array()) {
-        $url = "{$object->getServer()->url}/objects/{$object->pid}/datastreams/SCREEN/content";
+
+        // Construct the image URL.
+        $url = "{$object->getServer()->url}/objects/{$object->pid}" . 
+            "/datastreams/SCREEN/content";
+
+        // Return the image tag.
         return "<img class='fedora-renderer' alt='image' src='{$url}' />";
+
     }
+
 
 }
