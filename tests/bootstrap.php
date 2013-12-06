f971af2bc0db8c78b6b31b9d5f1129cc01905d18
David McClure <david.mcclure@virginia.edu>
2013-12-06 13:42:35 -0800

    Moves the base test case down into a 'cases' directory, to conform with other plugins.

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -15,4 +15,4 @@ define('OMEKA_DIR', dirname(dirname(FEDORA_DIR)));
 define('FEDORA_TEST_DIR', FEDORA_DIR.'/tests');
 
 require_once OMEKA_DIR.'/application/tests/bootstrap.php';
-require_once 'FedoraConnector_Test_AppTestCase.php';
+require_once 'cases/FedoraConnector_Test_AppTestCase.php';
