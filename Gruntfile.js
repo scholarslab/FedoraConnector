69f2543d5317ec96e15f77f7fbddda2be3cc7607
David McClure <david.mcclure@virginia.edu>
2014-01-29 19:51:55 -0800

    In plugin.ini title, adds space.

--- Gruntfile.js
+++ Gruntfile.js
@@ -11,9 +11,9 @@
 
 module.exports = function(grunt) {
 
-  grunt.loadNpmTasks('grunt-contrib-clean');
   grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-clean');
   grunt.loadNpmTasks('grunt-contrib-compress');
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-contrib-jasmine');
